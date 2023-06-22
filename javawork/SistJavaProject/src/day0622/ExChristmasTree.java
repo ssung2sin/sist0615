2f14fd2c6759dd9eb30b2442fc7e36c9e58d59c6
ssung2sin <ssung2sin@naver.com>
2023-06-22 17:27:43 +0900

    upload 0622-1

--- javawork/SistJavaProject/src/day0622/ExChristmasTree.java
+++ javawork/SistJavaProject/src/day0622/ExChristmasTree.java
@@ -1,20 +1,35 @@
 package day0622;
 
+import java.util.Scanner;
+
 public class ExChristmasTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i,j,k;
-		for(i=1;i<=5;i++) {
-			for(j=1;j<=(2*i-1);j++) {
-				for(k=1;k<=5-i;k++) {
-					if(j>=2)
-						continue;
-					System.out.print(" ");
-				}
-			System.out.print("*");
+		int i,star,space,stair;
+		
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+			System.out.println("만들고 싶은 크리스마스 트리 층수를 입력하세요 : (0 출력시 종료)");
+			stair=sc.nextInt();
+			if(stair==0) {
+				System.out.println("종료합니다.");
+				System.out.println();
+				break;
 			}
-		System.out.println();
+			for(i=1;i<=stair;i++) {
+			
+				for(star=1;star<=(2*i-1);star++) {
+				
+					for(space=1;space<=stair-i;space++) {
+						if(star>=2)
+							continue;
+						System.out.print(" ");
+					}
+					System.out.print("*");
+				}
+				System.out.println();
+			}
 		}
 
 	}
